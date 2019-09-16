634ff5dbe46021b9390e8e0918338458eb62cd7a
chloechengr <chengsn@bc.edu>
2019-09-16 14:30:39 -0400

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Chloe Cheng on 9/16/19.
+//  Copyright © 2019 Chloe Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
